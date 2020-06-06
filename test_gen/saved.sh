d5e44177b6bced6d25be54decbeadc196071521b
root <root@node-0.6g5.postman-pg0.wisc.cloudlab.us>
2020-06-05 21:46:08 -0500

    update

---
+++ test_gen/saved.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# show component init() stats
+for proj in mapreduce hadoop-tools yarn hdfs hbase; do echo "proj: $proj"; grep -rn ' init,' $proj/final/component/ | awk '{print $2}' | sort | uniq -c | sort -n -r -k1 | awk '{print $1" "$2}'; echo ''; done
+
+# get all get-parameter table
+for dir in $(find . -name parameter); do for i in $dir/*; do cat $i | awk '{print $5" "$3}'; done; done | sort -u > all_get_parameter.txt
+
+# get 5-xml get-parameter
+IFS=$'\n'; for line in $(cat all_get_parameter.txt); do para=$(echo $line | awk '{print $2}'); if [ "$(grep ^"$para"$ all_xml_parameters.txt)" != "" ]; then echo "$line"; fi; done 
+
+# test num for each parameter
+for i in *; do echo "$i $(cat $i | wc -l)"; done | sort -n -k2 -r
+
+# test num for each component
+cat * | awk '{print $4}' | sort  | uniq -c | sort -n -k1 -r
+
+# generate testing tuples
+for p in $(cat ./paras_this_round.txt); do ./generate_ff.sh $p > "$p".txt & pids+=($!); done; for id in ${pids[@]}; do wait $id; echo "$id done"; done
