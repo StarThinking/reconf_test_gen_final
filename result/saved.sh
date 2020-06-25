7c746e04e5d8f9428ae888cec5d270974fcda88c
root <root@node-0.controller.postman-pg0.wisc.cloudlab.us>
2020-06-25 13:35:21 -0500

    update

--- result/saved.sh
+++ result/saved.sh
@@ -3,5 +3,7 @@
 # find white list
 cf=0.9999; for hlog in getBoolean/*_hypothesis_*; do ~/parameter_test_controller/hypo_analysis.sh $hlog $cf 0; done | while read line; do ~/vm_images/the_final/result/extract_utility.sh $line para_and_comp; done
 
+mkdir no_need_hypo/; cf=0.9999; for hlog in *_hypothesis_*; do ~/parameter_test_controller/hypo_analysis.sh $hlog $cf 0; done | while read line; do mv $line no_need_hypo/; done
+
 # mkdir parameter%component
 for i in *; do p_and_c="$(../../extract_utility.sh $i 'para')%$(../../extract_utility.sh $i 'comp')"; if [ ! -d $p_and_c ]; then mkdir $p_and_c; fi; mv $i $p_and_c; done
