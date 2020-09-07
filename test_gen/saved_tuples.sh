f27785106b8ec0da0755a5da82f9f989cb0547af
root <root@node-0.final.postman-pg0.wisc.cloudlab.us>
2020-09-07 00:18:19 -0500

    rm db

--- test_gen/saved_tuples.sh
+++
@@ -1,2 +0,0 @@
-# generate components.txt (exclude OtherComponent) and parameters.txt for each test
-for i in *; do cat $i | awk '{print $1}' | sort -u > ../statistic/$i.parameters; done; for i in *; do cat $i | awk '{print $2}' | awk -F '.' '{print $1}' | grep -v 'OtherComponent' | sort -u > ../statistic/$i.components; done
